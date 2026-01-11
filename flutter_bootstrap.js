f683afa8bd521cf96e2cbdd6b12efadefdf7bb74
vin8003 <vin8003@gmail.com>
2026-01-11 12:44:07 +0530

    Deploy buyeasy_mobile web build (Standard) - Redeploy

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3572068413"
+    serviceWorkerVersion: "1661075978"
   }
 });
