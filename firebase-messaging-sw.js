9e25bf56a985b53de3dc7ab4b59c1e067be48c77
vin8003 <vin8003@gmail.com>
2026-01-11 16:46:09 +0530

    Deploy buyeasy_retailer web build (Standard) - Redeploy after updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,5 @@
 importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js");
-
 firebase.initializeApp({
     apiKey: "AIzaSyDL43Wm3WJwn7DysDb6kCfT425riQ7bytw",
     authDomain: "buyeasy-4003f.firebaseapp.com",
@@ -10,5 +9,4 @@ firebase.initializeApp({
     appId: "1:266120844334:web:0d9ba7f821b7447b9ebbbf",
     measurementId: "G-NYWF0GNCZ5"
 });
-
 const messaging = firebase.messaging();
