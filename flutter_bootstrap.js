3163cfdcad03d49308439ef23bec4ff228e39b90
vin8003 <vin8003@gmail.com>
2026-01-11 13:19:35 +0530

    Deploy buyeasy_mobile web build (Standard) - Fix token refresh v3

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2823228860"
+    serviceWorkerVersion: "3683056075"
   }
 });
