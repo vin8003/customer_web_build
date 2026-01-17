a4d657ea832305b195015ba21641c099744ed547
vin8003 <vin8003@gmail.com>
2026-01-17 10:31:04 +0530

    Deploy buyeasy_mobile web build (WASM) - Redeploy

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "484094221"
+    serviceWorkerVersion: "2930640606"
   }
 });
