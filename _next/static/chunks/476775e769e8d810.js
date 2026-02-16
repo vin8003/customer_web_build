3bf263ae37d3e185d3abd281a54e8a02a4c0d99d
shopeasy6994 <shopeasy.bte@gmail.com>
2026-02-16 17:07:39 +0530

    16/2/26

---
+++ _next/static/chunks/476775e769e8d810.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,52683,e=>{"use strict";var t=e.i(71645),u=e.i(18566);function r(){let e=(0,u.useRouter)();return(0,t.useEffect)(()=>{e.push("/retailers")},[e]),null}e.s(["default",()=>r])}]);
